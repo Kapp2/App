91fc95b252d634d8f7efc57bc7351a72006d29c0
PedroMsgr <pedrormsgr@gmail.com>
2023-02-19 21:46:27 +0100

    Revert "Paquetacion y boton prueba"
    This reverts commit fe77d968e2c9f63794563a4de12d91266172fec2.

--- app/src/main/java/com/example/kapp2/ui/favoritos/FavoritosViewModel.kt
+++ app/src/main/java/com/example/kapp2/ui/favoritos/FavoritosViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.kapp2.ui.dashboard
+package com.example.kapp2.ui.favoritos
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class FavoritosViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is favoritos Fragment"
     }
     val text: LiveData<String> = _text
 }
