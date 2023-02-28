f08fc09cba833a1ddc75e3fcf3b033f446653cb0
PedroMsgr <pedrormsgr@gmail.com>
2023-02-28 17:35:08 +0100

    Perfiles intento

--- app/src/main/java/com/example/kapp2/viewModel/AppViewModel.kt
+++ app/src/main/java/com/example/kapp2/viewModel/AppViewModel.kt
@@ -12,10 +12,19 @@ class AppViewModel(application: Application) : AndroidViewModel(application) {
 
     private val repositorio:Repository
     val botonesLiveData : LiveData<List<Boton>>
-    val favoritos
+    var favoritosLiveData =  MutableLiveData(0)
     val perfilesLiveData : LiveData<List<Perfil>>
     private val tematicaLiveData = MutableLiveData(5)
-
+    /*
+    val PERFIL="Pedro"
+    val TEMATICA="0"
+    private val filtrosLiveData by lazy {//inicio tardío
+        val mutableMap = mutableMapOf<String, Any?>(
+            PERFIL to null,
+            TEMATICA to 3
+        )
+        MutableLiveData(mutableMap)
+    }*/
     init {
         //inicia repositorio
         Repository(getApplication<Application>().applicationContext)
@@ -27,6 +36,31 @@ class AppViewModel(application: Application) : AndroidViewModel(application) {
             else
                 repositorio.getBotonesFiltroTematica(tematica)
         }
+        /*
+        botonesLiveData=Transformations.switchMap(filtrosLiveData)
+        { mapFiltro ->
+            val perfilSelected = mapFiltro!![PERFIL] as Perfil
+            val tema = mapFiltro!![TEMATICA] as Int
+            //Devuelve el resultado del when
+            when {//trae toda la lista de tareas
+                (perfilSelected == null && (tema == 0)) ->
+                    repositorio.getAllBotones()
+                //Sólo filtra por ESTADO
+                (perfilSelected == null && (tema != 0)) ->
+                    repositorio.getBotonesFiltroTematica(tema)
+                //Sólo filtra SINPAGAR
+                (perfilSelected != null) ->
+                    repositorio.getBotonesOfPerfil(perfilSelected)
+                //Filtra por ambos
+                else ->
+                    repositorio.getAllBotones()
+            }
+        }
+
+         */
+
+
+
     }
 
     fun setTematica(tematica: Int) {
